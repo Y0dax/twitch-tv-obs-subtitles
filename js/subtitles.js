d0f9cda935124064c10f78bf27ccfa1aee647385
David Myers <dave-myers@outlook.com>
2020-10-24 15:38:38 -0400

    reduce changes

--- js/subtitles.js
+++ js/subtitles.js
@@ -1,203 +1,199 @@
-(e => {
-    'use strict';
+(e=>{'use strict';
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Settings
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    const defaultSubkey = 'sub-c-79b0a26a-80a9-11e8-8f4a-96bbd71e7d14';
-    const defaultPubkey = 'pub-c-fd9b97a4-7b78-4ae1-a21e-3614f2b6debe';
-    const defaultChannel = uuid();
-    const defaultMaxWords = 250;
-    const defaultStyle = '';
-    const clearTime = +uripart('cleartime') || 2.5; // Seconds
-    const introText = uripart('introtext') || 'Start talking.';
-    const continuous = uripart('continuous') || 'on';
-    const mic = uripart('mic') || 'on';
-    const language = uripart('language') || uripart('lang') || null;
-    const subkey = uripart('subkey') || defaultSubkey;
-    const pubkey = uripart('pubkey') || defaultPubkey;
-    const channel = uripart('channel') || username() || askchannel() || defaultChannel;
-    const maxWords = uripart('maxwords') || defaultMaxWords;
-    let subtitleStyle = uripart('style') || defaultStyle;
-    var botChannel = new BroadcastChannel('bot_bus');
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Settings
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+const defaultSubkey   = 'sub-c-79b0a26a-80a9-11e8-8f4a-96bbd71e7d14';
+const defaultPubkey   = 'pub-c-fd9b97a4-7b78-4ae1-a21e-3614f2b6debe';
+const defaultChannel  = uuid();
+const defaultMaxWords = 250;
+const defaultStyle    = '';
+const clearTime       = +uripart('cleartime') || 2.5; // Seconds
+const introText       = uripart('introtext')  || 'Start talking.';
+const continuous      = uripart('continuous') || 'on';
+const mic             = uripart('mic')        || 'on';
+const language        = uripart('language')   || uripart('lang') || null;
+const subkey          = uripart('subkey')     || defaultSubkey;
+const pubkey          = uripart('pubkey')     || defaultPubkey;
+const channel         = uripart('channel')    || username() || askchannel() || defaultChannel;
+const maxWords        = uripart('maxwords')   || defaultMaxWords;
+let   subtitleStyle   = uripart('style')      || defaultStyle;
+const botChannel = new BroadcastChannel('bot_bus');
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Ask for Channel
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    function askchannel() {
-        let instructions =
-            "Missing configuration!<br>" +
-            "<a href='https://github.com/stephenlb/twitch-tv-obs-subtitles/" +
-            "blob/master/readme.md#running-twitchtv-subtitles-from-local-" +
-            "files-on-your-hard-drive'>Follow Instructions</a>";
-        setTimeout(e => candidate(instructions), 1000);
-    }
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Ask for Channel
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function askchannel() {
+    let instructions                                                     = 
+        "Missing configuration!<br>"                                     +
+        "<a href='https://github.com/stephenlb/twitch-tv-obs-subtitles/" + 
+        "blob/master/readme.md#running-twitchtv-subtitles-from-local-"   +
+        "files-on-your-hard-drive'>Follow Instructions</a>";
+    setTimeout( e => candidate(instructions), 1000 );
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Introduction Text
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    setTimeout(e => candidate(introText), 10);
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Introduction Text
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+setTimeout( e => candidate(introText), 10 );
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // UI Elements
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    let subtitles = document.querySelector('#subtitle');
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// UI Elements
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+let subtitles = document.querySelector('#subtitle');
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Main
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    async function main() {
-        // Listen for OBS Updates
-        startSubscribe(channel, speech => updateSubtitles(speech));
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Main
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+async function main() {
+    // Listen for OBS Updates
+    startSubscribe( channel, speech => updateSubtitles(speech) );
 
-        // Set Styles of Subtiltle Text
-        updateSubtitleStyle(subtitleStyle);
+    // Set Styles of Subtiltle Text
+    updateSubtitleStyle(subtitleStyle);
 
-        // Set Language
-        if (language) spoken.recognition.lang = language;
+    // Set Language
+    if (language) spoken.recognition.lang = language;
 
-        // Listen to Microphone
-        if (mic == 'off') return;
+    // Listen to Microphone
+    if (mic == 'off') return;
 
-        // Listen for Words
-        listen();
+    // Listen for Words
+    listen();
 
-        // Continuous Listening
-        spoken.listen.on.end(listen);
-        spoken.listen.on.error(listen);
+    // Continuous Listening
+    spoken.listen.on.end(listen);
+    spoken.listen.on.error(listen);
 
-        // Search Giphy Image
-        spoken.listen.on.partial(candidate);
-    }
+    // Search Giphy Image
+    spoken.listen.on.partial(candidate);
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Word Search Candidate
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    function candidate(speech) {
-        publish(channel, {
-            phrase: updateSubtitles({ phrase: speech })
-            , style: subtitleStyle
-        });
-    }
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Word Search Candidate
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function candidate(speech) {
+    publish( channel, {
+        phrase : updateSubtitles({ phrase: speech })
+    ,   style  : subtitleStyle
+    } );
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Update Subtitles Style
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    function updateSubtitleStyle(style) {
-        subtitles.style = style;
-    }
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Update Subtitles Style
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function updateSubtitleStyle(style) {
+    subtitles.style = style;
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Update Subtitles
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    function updateSubtitles(speech) {
-        if (speech && speech['style']) subtitleStyle = speech['style'];
-        updateSubtitleStyle(subtitleStyle);
-        speech.style = subtitleStyle;
-        subtitles.innerHTML = getMaxWords(speech.phrase);
-        //botChannel.postMessage(speech.phrase);
-            
-        // Clear Text after moments of silence.
-        clearTimeout(updateSubtitles.ival);
-        updateSubtitles.ival = setTimeout(async ival => {
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Update Subtitles
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function updateSubtitles(speech) {
+    if (speech && speech['style']) subtitleStyle = speech['style'];
+    updateSubtitleStyle(subtitleStyle);
+    speech.style = subtitleStyle;
+    subtitles.innerHTML = getMaxWords(speech.phrase);
+
+    // Clear Text after moments of silence.
+    clearTimeout(updateSubtitles.ival);
+    updateSubtitles.ival = setTimeout( async ival => {
             botChannel.postMessage(speech.phrase); //Broadcast message for bot to pick up if running.
             subtitles.innerHTML = ' ';
             spoken.listen.stop();
             listen();
-        }, (+clearTime) * 1000);
+    }, (+clearTime) * 1000 );
 
-        return subtitles.innerHTML;
-    }
+    return subtitles.innerHTML;
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Ensure only maxWords are displayed on the screen
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    function getMaxWords(speech) {
-        let words = speech.split(' ');
-        return words.slice(-maxWords).join(' ');
-    }
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Ensure only maxWords are displayed on the screen
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function getMaxWords(speech) {
+    let words = speech.split(' ');
+    return words.slice(-maxWords).join(' ');
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Listen for Voice Commands
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    async function listen() {
-        await delay(200);
-        spoken.listen({ continuous: continuous == 'on' }).then(speech => {
-            candidate(speech);
-            used = {};
-        }).catch(e => true);
-    }
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Listen for Voice Commands
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+async function listen() {
+    await delay(200);
+    spoken.listen({continuous:continuous=='on'}).then( speech => {
+        candidate(speech);
+        used = {};
+    } ).catch( e => true );
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Easy Wait Command
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    function delay(duration) {
-        return new Promise(resolve => setTimeout(resolve, duration));
-    }
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Easy Wait Command
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function delay(duration) {
+    return new Promise( resolve => setTimeout( resolve, duration ) );
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Get URI Parameters
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    function uripart(key) {
-        const params = {};
-        const href = location.href;
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Get URI Parameters
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function uripart(key) {
+    const params = {};
+    const href   = location.href;
 
-        if (href.indexOf('?') < 0) return '';
+    if (href.indexOf('?') < 0) return '';
 
-        href.split('?')[1].split('&').forEach(m => {
-            const kv = m.split('=');
-            params[kv[0]] = kv[1];
-        });
+    href.split('?')[1].split('&').forEach( m => {
+        const kv = m.split('=');
+        params[kv[0]] = kv[1];
+    } );
 
-        if (key in params) return decodeURIComponent(params[key]);
+    if (key in params) return decodeURIComponent(params[key]);
 
-        return '';
-    }
+    return '';
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Subscribe for OBS Updates
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    function startSubscribe(channelName, callback) {
-        subscribe({
-            subkey: subkey
-            , channel: channelName
-            , message: payload => {
-                payload.m.forEach(message => callback(message.d));
-            }
-        });
-    }
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Subscribe for OBS Updates
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function startSubscribe( channelName, callback ) {
+    subscribe({
+        subkey  : subkey
+    ,   channel : channelName
+    ,   message : payload => {
+            payload.m.forEach( message => callback(message.d) );
+        }
+    });
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Publish Captured Subtitles
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    const origin = 'ps' + (Math.random() + '').split('.')[1] + '.pubnub.com';
-    function publish(channelName, data = {}) {
-        return requester({ timeout: 10000 })({
-            url: [
-                'https://', origin,
-                '/publish/', pubkey,
-                '/', subkey,
-                '/0/', channelName,
-                '/0/', encodeURIComponent(JSON.stringify(data))
-            ].join('')
-        });
-    }
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Publish Captured Subtitles
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+const origin = 'ps'+(Math.random()+'').split('.')[1]+'.pubnub.com';
+function publish( channelName, data={} ) {
+    return requester({ timeout : 10000 })({ url : [
+        'https://',  origin,
+        '/publish/', pubkey,
+        '/',         subkey,
+        '/0/',       channelName,
+        '/0/',       encodeURIComponent(JSON.stringify(data))
+    ].join('') });
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // UUID
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    function uuid() {
-        let u = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
-            function (c) {
-                let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-                return v.toString(16);
-            });
-        return u;
-    }
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// UUID
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function uuid() {
+    let u = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+    function(c) {
+        let r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+    return u;
+}
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Run Main Function
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    main();
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Run Main Function
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+main();
 
 })();
